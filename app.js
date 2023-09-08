7ccb5618cb858d1bbdea18454bcc0d0686aa181d
Anuja Anuja <anujabarin@Anujas-Air.lan>
2023-09-08 14:54:11 -0500

    made changes in the string

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const text = [
-  `USA Today is known for news in , easy-to-read-and-comprehend stories. In the main edition circulated in the United States and Canada, each edition consists of four sections: News (the "front page" section), Money, Sports, and Life. Since March 1998, the Friday edition of Life has been split into two sections: the regular Life focusing on entertainment (subtitled Weekend; section E), which features television reviews and listings, a DVD column, film reviews and trends, and a travel supplement called Destinations & Diversions (section D). The international edition of the paper features two sections: News and Money in one; with Sports and Life in the other.`,
+  `USA Today is known for news in comment, easy-to-read-and-comprehend stories. In the main edition circulated in the United States and Canada, each edition consists of four sections: News (the "front page" section), Money, Sports, and Life. Since March 1998,
+   the Friday edition of Life has been split into two 
+   sections: the regular Life focusing on entertainment (subtitled Weekend; section E)
+   , which features television reviews and listings, a DVD column, film reviews and trends, 
+   and a travel supplement called Destinations & Diversions (section D). The international 
+   edition of the paper features two sections: News and Money in one; with Sports and Life in the other.so this is the news`,
 ];
 
 const form = document.querySelector(".lorem-form");
